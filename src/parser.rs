d78a177aa162b974ebc7438c2d27af1f3e59cfa5
Bryce Stabenow <brycestabenow617@gmail.com>
2024-04-04 07:35:20 -0500

    abstract classes for parser

--- src/parser.rs
+++ src/parser.rs
@@ -8,4 +8,12 @@ impl Parser {
     pub fn program(&self) {
         println!("parsing!")
     }
+
+    pub fn check_token(&self) {}
+
+    pub fn check_peek(&self) {}
+
+    pub fn match_token(&self) {}
+
+    pub fn next_token(&self) {}
 }
